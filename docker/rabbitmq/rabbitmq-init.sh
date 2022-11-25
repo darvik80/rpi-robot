35bc7bfbcb2e3309ec3eab72b9c4512b961515d9
Ivan Kishchenko <ivan.kishchenko@lazada.com>
2022-11-25 17:06:54 +0700

    docker-compose.yml

---
+++ docker/rabbitmq/rabbitmq-init.sh
@@ -0,0 +1,23 @@
+##!/bin/sh
+
+# Create Rabbitmq user
+( echo "Waiting for process to start..."; \
+sleep 30 ; \
+rabbitmqctl add_user $RABBITMQ_ADMIN_USER $RABBITMQ_ADMIN_PASSWORD 2>/dev/null ; \
+rabbitmqctl set_user_tags $RABBITMQ_ADMIN_USER administrator ; \
+rabbitmqctl set_permissions -p / $RABBITMQ_ADMIN_USER  ".*" ".*" ".*" ; \
+rabbitmqctl add_user $RABBITMQ_API_USER $RABBITMQ_API_PASSWORD 2>/dev/null ; \
+rabbitmqctl set_user_tags $RABBITMQ_API_USER management ; \
+rabbitmqctl set_permissions -p / $RABBITMQ_API_USER  ".*" ".*" ".*" ; \
+rabbitmqctl add_user $RABBITMQ_MQTT_USER $RABBITMQ_MQTT_PASSWORD 2>/dev/null ; \
+rabbitmqctl set_user_tags $RABBITMQ_MQTT_USER management ; \
+rabbitmqctl set_permissions -p / $RABBITMQ_MQTT_USER  ".*" ".*" ".*" ; \
+rabbitmqctl delete_user guest ; \
+echo "*** Admin User: '$RABBITMQ_ADMIN_USER' with password '$RABBITMQ_ADMIN_PASSWORD' completed. ***" ; \
+echo "*** API User:  '$RABBITMQ_API_USER' with password '$RABBITMQ_API_PASSWORD' completed. ***" ; \
+echo "*** Log in the WebUI at port 15672 (example: http:/localhost:15672) ***") &
+
+# $@ is used to pass arguments to the rabbitmq-server command.
+# For example if you use it like this: docker run -d rabbitmq arg1 arg2,
+# it will be as you run in the container rabbitmq-server arg1 arg2
+rabbitmq-server $@
