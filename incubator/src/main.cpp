6b4d98323fedb90f570aa349a2c6bc06b9781177
Ivan Kishchenko <ivan.kishchenko@lazada.com>
2023-05-28 21:32:17 +0700

    bitmask_operators.hpp

--- incubator/src/main.cpp
+++ incubator/src/main.cpp
@@ -1,112 +1,94 @@
 //
 // Created by Kishchenko Ivan on 27/11/2022.
 //
-#include <gpiod.hpp>
 
 #include "logging/Logging.h"
+#include "bitmask_operators.hpp"
+#include <system_error>
 
-#ifndef    CONSUMER
-#define    CONSUMER    "Consumer"
-#endif
+namespace iot {
+    enum class errc {
+        success = 0,
+        device_unknown,
+        device_auth_failed
+    };
 
-#include <pthread.h>
+    namespace detail {
 
-#include "gpiod/pwm.h"
+        class iot_category : public std::error_category {
+        public:
+            const char *name() const noexcept override {
+                return "my app";
+            }
 
-void *threadFunc(void *arg) {
+            [[nodiscard]] std::string message(int ev) const override {
+                switch (static_cast<iot::errc>(ev)) {
+                    case iot::errc::success:
+                        return "SUCCESS";
+                    case iot::errc::device_unknown:
+                        return "device unknown";
+                    case iot::errc::device_auth_failed:
+                        return "device auth failed";
+                    default:
+                        return "unknown";
+                }
+            }
 
-    int *stop = (int *) (arg);
-//    int policy;
-//    struct sched_param param{};
-//
-//    pthread_getschedparam(pthread_self(), &policy, &param);
-//    param.sched_priority = sched_get_priority_max(policy);
-//    pthread_setschedparam(pthread_self(), policy, &param);
-//
-    gpiod::chip chip("gpiochip0");
-    logger::info("chip: {}", chip.name());
-    logger::info("label: {}", chip.label());
-    logger::info("num_lines: {}", chip.num_lines());
-
-    gpiod::line trig = chip.get_line(4);
-    trig.request(
-            {
-                    "incubator",
-                    gpiod::line_request::DIRECTION_OUTPUT,
-                    0
-            },
-            0
-    );
-
-    gpiod::line echo = chip.get_line(17);
-    echo.request(
-            {
-                    "incubator",
-                    gpiod::line_request::DIRECTION_INPUT,
-                    0
-            },
-            0
-    );
-
-    trig.set_value(0);
-    usleep(2);
-    while (!(*stop)) {
-        trig.set_value(1);
-        usleep(10);
-        trig.set_value(0);
-
-        while (!echo.get_value()) {}
-        auto start = std::chrono::high_resolution_clock::now().time_since_epoch();
-        while (echo.get_value()) {}
-        auto end = std::chrono::high_resolution_clock::now().time_since_epoch();
-        auto distance = (double) (end - start).count() * 0.0343 / 2000;
-        logger::info("Dist: {}", distance);
-
-        sleep(1);
+            [[nodiscard]] bool equivalent(int code, const std::error_condition &condition) const _NOEXCEPT override {
+                return error_category::equivalent(code, condition);
+            }
+        };
     }
 
-    return nullptr;
+
+    const std::error_category &category() {
+        // The category singleton
+        static detail::iot_category instance;
+        return instance;
+    }
+
+    inline std::error_code make_error_code(iot::errc val) {
+        // Create an error_code with the original mpg123 error value
+        // and the mpg123 error category.
+        return {static_cast<int>(val), category()};
+    }
+
+    std::error_condition make_error_condition(iot::errc val) {
+        return {static_cast<int>(val), category()};
+    }
 }
 
-#include <thread>
+namespace std {
+    template<>
+    struct is_error_code_enum<iot::errc> : public true_type {
+    };
 
-#define PWMB1   16 //   20  28  26
-#define PWMB2   20 //   21  29  27
-#define PWM2    21 //   26  25  25
+    template<>
+    struct is_error_condition_enum<iot::errc> : public true_type {
+    };
+}
+
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-    std::thread thread;
-    logger::LoggingProperties logProps;
-    logProps.level = "info";
-    logger::setup(logProps);
-    volatile int stop = 0;
-    pthread_t threadId;
-    int err = pthread_create(&threadId, nullptr, &threadFunc, (void *) &stop);
+    ERenderPass primary = ERenderPass::Geometry | ERenderPass::Lighting;
 
+    if ((primary & ERenderPass::Geometry) == ERenderPass::Geometry) {
+        std::cout << "ERenderPass::Geometry" << std::endl;
+    }
 
-    err = pthread_join(threadId, nullptr);
+    auto ec1 = iot::make_error_code(iot::errc::device_unknown);
+    logger::info("val: {}", ec1.value());
+    logger::info("msg: {}", ec1.message());
 
-//    gpiod::chip chip("gpiochip0");
-//    logger::info("chip: {}", chip.name());
-//    logger::info("label: {}", chip.label());
-//    logger::info("num_lines: {}", chip.num_lines());
-//
-//    auto pwma1 = chip.get_line(PWMB1);
-//    pwma1.request({"incubator", gpiod::line_request::DIRECTION_OUTPUT,0 },0);
-//    auto pwma2 = chip.get_line(PWMB2);
-//    pwma2.request({"incubator", gpiod::line_request::DIRECTION_OUTPUT,0 },0);
-//    auto pwma = chip.get_line(PWM2);
-//    pwma.request({"incubator", gpiod::line_request::DIRECTION_OUTPUT,0 },0);
-//
-//    gpiod::extension::soft_pwm pwm(chip.get_line(PWM2), 0, 1024);
-//
-//    pwma1.set_value(1);
-//    pwma2.set_value(0);
-//
-//    pwm.write(1024);
-//    sleep(10);
-//    pwm.write(512);
-//    sleep(10);
-//    pwm.write(0);
+    auto ec2 = iot::make_error_code(iot::errc::device_auth_failed);
+
+    logger::info("val: {}", ec2.value());
+    logger::info("msg: {}", ec2.message());
+
+    if (ec1 == ec2) {
+        logger::info("same errors");
+    }
+
     return 0;
 }
